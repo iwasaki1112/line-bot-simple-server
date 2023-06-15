772acae32fe0e9b1c5461d4f32de6a830b6d71e6
iwasaki1112 <s.iwasaki1112@gmail.com>
2023-06-15 21:35:58 +0900

    - Updated LineBotSimpleServer class.

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,108 @@
-import { middleware, ClientConfig, MiddlewareConfig } from "@line/bot-sdk"
+import { middleware, ClientConfig, MiddlewareConfig, Client, Profile } from "@line/bot-sdk"
 import { FlexMessage, Event } from "./types";
-import express from 'express'
-
+import express, { Express } from 'express'
 const https = require('node:https');
-const config: ClientConfig = {
-  channelAccessToken: 'nJ0SE5VehIKpfDfMnq79ijs2XS7U0FE67OTv973quAVHGQWA8kEzVdh3/fETMrHMbEZQ3bYxVS+eSzc8I/iCiecWgzU/7NlWWRma1REKYaMuxOLTDLlt9+6Xuybe78fqVtF5siIDrQ0q1G3uGz7voAdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '390bd59925d724b635056d4eebbcb634'
-}
-const middleWareConfig: MiddlewareConfig = {
-  channelSecret: config.channelSecret!
-}
-const app = express()
 
-const message: FlexMessage = {
-  "type": "flex",
-  "altText": "This is a Flex Message",
-  "contents": {
-    "type": "bubble",
-    "body": {
-      "type": "box",
-      "layout": "horizontal",
-      "contents": [
-        {
-          "type": "text",
-          "text": "Hello,"
-        }
-      ]
+export class LineBotSimpleServer {
+  private _app: Express
+  private _message: FlexMessage | null = null
+
+  constructor(port: string, channelAccessToken: string, channelSecret: string) {
+    this._app = express()
+
+    if (!channelAccessToken) throw new Error(`there isn't channelAccessToken`)
+    if (!channelSecret) throw new Error(`there isn't channelSecret`)
+
+    const config: ClientConfig = {
+      channelAccessToken: process.env.CHANNEL_ACCESS_TOKE!,
+      channelSecret: process.env.CHANNEL_SECRET,
     }
+    const middleWareConfig: MiddlewareConfig = {
+      channelSecret: config.channelSecret!
+    }
+
+    this.startServer(port, config, middleWareConfig)
+  }
+
+  async startServer(port: string, config: ClientConfig, middleWareConfig: MiddlewareConfig) {
+
+    this._app.listen(port, () => {
+      console.log(`App listening at http://localhost:${port}`);
+    })
+
+    this._app.post('/webhook', middleware(middleWareConfig), async (req, res, next) => {
+      try {
+        if (!this._message) throw new Error(`It's neccesary to set message. but there isnt message`)
+        const events: Event[] = req.body.events
+        const userId: string = events[0].source.userId
+        const profile = await this._getProfile(userId, config)
+        const replacedMessage = this._replacePlaceholder(message, profile)
+
+        const dataStirng = JSON.stringify({
+          replyToken: events[0].replyToken,
+          messages: [replacedMessage]
+        })
+
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${config.channelAccessToken}`
+        }
+
+        const webhookOptions = {
+          hostname: 'api.line.me',
+          path: '/v2/bot/message/reply',
+          method: 'POST',
+          headers: headers,
+          body: dataStirng
+        }
+
+        const request = https.request(webhookOptions, (res: any) => {
+          res.on('data', (d: any) => {
+            process.stdout.write(d)
+          })
+        })
+
+        request.on('error', (err: any) => {
+          console.error(err)
+        })
+
+        request.write(dataStirng)
+        request.end()
+      } catch (err) {
+        console.error(err)
+        res.status(500).send(err)
+      }
+    })
+
+  }
+
+  /**
+   * this method makes some placeholder to replace.
+   * @param {FlexMessage} message
+   * @param {Profile} profile 
+   * @returns 
+   */
+  _replacePlaceholder(message: FlexMessage, profile: Profile): FlexMessage {
+    let messageStr = JSON.stringify(message);
+    messageStr = messageStr.replace('[displayName]', profile.displayName);
+
+    const updatedMessage: FlexMessage = JSON.parse(messageStr);
+    return updatedMessage
+  }
+
+  /**
+   * Get user profile who sent to bot.
+   * @param {string} userId 
+   * @param {ClientConfig} config 
+   * @returns 
+   */
+  async _getProfile(userId: string, config: ClientConfig): Promise<Profile> {
+    const client = new Client(config)
+    const profile = await client.getProfile(userId)
+    return profile
+  }
+
+  setMessage(message: FlexMessage) {
+    this._message = message
   }
 }
-
-app.post('/webhook', middleware(middleWareConfig), (req, res) => {
-  const events: Event[] = req.body.events
-  const dataStirng = JSON.stringify({
-    replyToken: events[0].replyToken,
-    messages: [message]
-  })
-
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${config.channelAccessToken}`
-  }
-
-  const webhookOptions = {
-    hostname: 'api.line.me',
-    path: '/v2/bot/message/reply',
-    method: 'POST',
-    headers: headers,
-    body: dataStirng
-  }
-
-  const request = https.request(webhookOptions, (res: any) => {
-    res.on('data', (d: any) => {
-      process.stdout.write(d)
-    })
-  })
-
-  request.on('error', (err: any) => {
-    console.error(err)
-  })
-
-  request.write(dataStirng)
-  request.end()
-})
-
-app.listen('8081', () => {
-  console.log("Example app listening at http://localhost:8081");
-})
